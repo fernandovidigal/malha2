947679783aef65b5d49c2441076e5450390970cf
Fernando <fernandovidigal@gmail.com>
2020-02-26 15:05:14 +0000

    Package Updates

--- helpers/webApi.js
+++ helpers/webApi.js
@@ -16,18 +16,21 @@ async function syncLocalidades(url){
         const localidadesWeb = responseWeb.data.data;
 
         // Remove das listas as localidades que já estão sincronizadas
-        const localidadesComDefeitoDeSincronizacao = [];
         const localidadesAppSincronizar = [];
-        localidadesApp.forEach((lapp, lappIndex) => {
-            const index = localidadesWeb.findIndex(lweb => lweb.nome == lapp.nome && lweb.syncHash == lapp.syncHash);
+        const localidadesAppComDefeito = [];
+        const localidadesWebComDefeito = [];
+        localidadesApp.forEach(lapp => {
+            const index = localidadesWeb.findIndex(lweb => lweb.syncHash == lapp.syncHash);
             // Foi encontrado um elemento igual
             if(index != -1) {
                 // Verifica se a localidade tem defeitos na sincronização, ou seja, algum dos elementos syncWeb ou syncApp não é 1
-                if(lapp.syncApp != 1 || lapp.syncWeb != 1 || localidadesWeb[index].syncApp != 1 || localidadesWeb[index].syncWeb != 1){
-                    const localidadesComDefeito = [];
-                    localidadesComDefeito.push(lapp);
-                    localidadesComDefeito.push(localidadesWeb[index]);
-                    localidadesComDefeitoDeSincronizacao.push(localidadesComDefeito);
+                // Verifica primeiro na Aplicação
+                if(lapp.syncApp != 1 || lapp.syncWeb != 1){
+                    localidadesAppComDefeito.push(lapp);
+                }
+                // Verifica na web
+                if(localidadesWeb[index].syncApp != 1 || localidadesWeb[index].syncWeb != 1){
+                    localidadesWebComDefeito.push(localidadesWeb[index]);
                 }
                 // Se tiver defeito de sincronização a localidade já está na array localidadesComDefeitoDeSincronizacao
                 // Se não tiver defeito de sincronização é removida da array localidadesWeb, uma vez que já está sincronizada
@@ -40,48 +43,46 @@ async function syncLocalidades(url){
         });
 
         // Corrige defeito na sincronização
-        for(const parLocalidades of localidadesComDefeitoDeSincronizacao){
-            const _localidadeApp = parLocalidades[0];
-            const _localidadeWeb = parLocalidades[1];
-
-            if(_localidadeApp.syncApp !=1 || _localidadeApp.syncWeb != 1){
-                await dbFunctions.updateLocalidadeSync(_localidadeApp.localidadeId, 1, 1);
-            }
-
-            if(_localidadeWeb.syncApp !=1 || _localidadeWeb.syncWeb != 1){
-                await axios.post(`${url}api/localidades/updateSync.php?key=${apiKey}`, {
-                    localidadeId: _localidadeWeb.localidadeId,
-                    appValue: 1,
-                    webValue: 1
-                });
-            }
-        }
+        // Percorre, primeiro da App depois da Web, as listas de localidades com defeito e adiciona-as ao array localidadesASincronizar
+        // para serem executadas paralelamente
+        const localidadesASincronizar = [];
+        localidadesAppComDefeito.forEach(_localidade => {
+            localidadesASincronizar.push(dbFunctions.updateLocalidadeSync(_localidade.localidadeId, 1, 1));
+        });
+        localidadesWebComDefeito.forEach(_localidade => {
+            localidadesASincronizar.push(axios.post(`${url}api/localidades/updateSync.php?key=${apiKey}`, {
+                localidadeId: _localidade.localidadeId,
+                appValue: 1,
+                webValue: 1
+            }));
+        });
 
         if(localidadesAppSincronizar.length > 0){
             for(const localidade of localidadesAppSincronizar){
                 // Se na app existe localidade com 1 1, então é porque na web foi apaga a localidade
+                // ou seja, não foi encontrada na web localidade igual, mas na App a localidades está como estando sincronizada
                 if(localidade.syncApp == 1 && localidade.syncWeb == 1){
                     const numEquipas = await dbFunctions.getNumEquipasPorFiltro({ localidadeId: localidade.localidadeId });
                     // Se existem equipas na app não se pode apagar a localidade, então regista a localidade na web
                     if(numEquipas > 0){
-                        await axios.post(`${url}api/localidades/create.php?key=${apiKey}`, {
+                        localidadesASincronizar.push(axios.post(`${url}api/localidades/create.php?key=${apiKey}`, {
                             nome: localidade.nome,
                             syncHash: localidade.syncHash,
                             appValue: 1,
                             webValue: 1
-                        });
+                        }));
                     } else {
-                        // Não existem equipas na App para a dada localidade, apagar localidade na App
-                        await dbFunctions.deleteLocalidade(localidade.localidadeId);
+                        // Não existem equipas na App para a dada localidade, apagar localidade também na App
+                        localidadesASincronizar.push(dbFunctions.deleteLocalidade(localidade.localidadeId));
                     }
                 } else {
-                    await axios.post(`${url}api/localidades/create.php?key=${apiKey}`, {
+                    localidadesASincronizar.push(axios.post(`${url}api/localidades/create.php?key=${apiKey}`, {
                         nome: localidade.nome,
                         syncHash: localidade.syncHash,
                         appValue: 1,
                         webValue: 1
-                    });
-                    await dbFunctions.updateLocalidadeSyncWeb(localidade.localidadeId, 1);
+                    }));
+                    localidadesASincronizar.push(dbFunctions.updateLocalidadeSyncWeb(localidade.localidadeId, 1));
                 }
             }
         }
@@ -91,21 +92,25 @@ async function syncLocalidades(url){
                 if(localidade.syncApp == 1 && localidade.syncWeb == 1){
                     const response = await axios.get(`${url}api/equipas/numEquipas.php?key=${apiKey}&localidadeId=${localidade.localidadeId}`);
                     if(response.data.numEquipas > 0) {
-                        await dbFunctions.createLocalidade(localidade.nome, localidade.syncHash, localidade.syncApp, localidade.syncWeb);
+                        localidadesASincronizar.push(dbFunctions.createLocalidade(localidade.nome, localidade.syncHash, localidade.syncApp, localidade.syncWeb));
                     } else {
-                        await axios.post(`${url}api/localidades/delete.php?key=${apiKey}`, {
+                        localidadesASincronizar.push(axios.post(`${url}api/localidades/delete.php?key=${apiKey}`, {
                             localidadeId: localidade.localidadeId,
-                        });
+                        }));
                     }
                 } else {
-                    await axios.post(`${url}api/localidades/updateSyncApp.php?key=${apiKey}`, {
+                    localidadesASincronizar.push(axios.post(`${url}api/localidades/updateSyncApp.php?key=${apiKey}`, {
                         localidadeId: localidade.localidadeId,
                         value: 1
-                    });
-                    await dbFunctions.createLocalidade(localidade.nome, localidade.syncHash, 1, 1);
+                    }));
+                    localidadesASincronizar.push(dbFunctions.createLocalidade(localidade.nome, localidade.syncHash, 1, 1));
                 }
             }
         }
+
+        // Sincroniza todas as localidades paralelamente
+        await Promise.all(localidadesASincronizar);
+
     } catch(error) {
         return false;
     }
